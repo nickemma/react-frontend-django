9705dc87dccb63e27d629250e066b3d28697e5b7
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-12 12:29:11 +0100

    getting started with tailwind

--- react-master/src/App.js
+++ react-master/src/App.js
@@ -1,20 +1,53 @@
 import Employee from './components/Employee';
 import './index.css';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [Employees, setEmployees] = useState([
+    {
+      name: 'John Doe',
+      role: 'developer',
+      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+    },
+    {
+      name: 'Oyin Lizdev',
+      role: 'developer',
+      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+    },
+    {
+      name: 'Said Lassri',
+      role: 'Engineer',
+      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+    },
+    {
+      name: 'Jane Doe',
+      role: 'Manager',
+      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+    },
+    {
+      name: 'Sidney Kaguli',
+      role: 'developer',
+      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+    },
+    {
+      name: 'Techie Emma',
+      role: 'Engineer',
+      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+    },
+  ]);
 
   return (
-    <div className="App">
-      <input
-        type="text"
-        value={role}
-        onChange={(e) => setRole(e.target.value)}
-      />
-      <Employee name="john" role="intern" />
-      <Employee name="oyin" role={role} />
-      <Employee name="said" />
+    <div className="flex flex-wrap justify-center">
+      {Employees.map((employee) => (
+        <Employee
+          key={uuidv4()}
+          name={employee.name}
+          role={employee.role}
+          img={employee.img}
+        />
+      ))}
     </div>
   );
 }
--- react-master/src/components/Employee.jsx
+++ react-master/src/components/Employee.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
-const Employee = ({ name, role }) => {
+const Employee = ({ name, role, img }) => {
   return (
     <>
-      <h1>Employee {name}</h1>
-      <p>{role ? role : 'no role'}</p>
+      <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img
+          className="object-cover h-[100px] w-[100px] block mx-auto rounded-full sm:mx-0 sm:shrink-0"
+          src={img}
+        />
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">{name}</p>
+            <p className="text-slate-500 font-medium">{role}</p>
+          </div>
+          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Edit
+          </button>
+        </div>
+      </div>
     </>
   );
 };
