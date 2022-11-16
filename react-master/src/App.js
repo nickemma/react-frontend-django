fa9d58e5defa8ee9dce15a1ef301e4c413e84f69
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-16 17:56:36 +0100

    updated the images

--- react-master/src/App.js
+++ react-master/src/App.js
@@ -11,37 +11,37 @@ function App() {
       id: 1,
       name: 'John Doe',
       role: 'developer',
-      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+      img: 'https://lazy-coders.netlify.app/static/media/trainer3.1b157a017adb37741dd7.png',
     },
     {
       id: 2,
       name: 'Oyin Lizdev',
       role: 'developer',
-      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+      img: 'https://lazy-coders.netlify.app/static/media/avatar4.a56d66be082fdec88e25.jpg',
     },
     {
       id: 3,
       name: 'Said Lassri',
       role: 'Engineer',
-      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+      img: 'https://lazy-coders.netlify.app/static/media/trainer4.edd3a24a262a97747b1b.png',
     },
     {
       id: 4,
       name: 'Jane Doe',
       role: 'Manager',
-      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+      img: 'https://lazy-coders.netlify.app/static/media/trainer11.f65ad628e627ba4d6c24.png',
     },
     {
       id: 5,
       name: 'Sidney Kaguli',
       role: 'developer',
-      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+      img: 'https://lazy-coders.netlify.app/static/media/trainer5.d8c1fc0f303466786921.png',
     },
     {
       id: 6,
       name: 'Techie Emma',
       role: 'Engineer',
-      img: 'https://images.pexels.com/photos/3201630/pexels-photo-3201630.jpeg',
+      img: 'https://lazy-coders.netlify.app/static/media/trainer8.3dffb89dfbea9d7201ce.png',
     },
   ]);
 
